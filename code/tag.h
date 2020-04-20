d9ac8b74e91df67b1eb1ad4ab225b9c4782a481f
Hugo <hugo.vovard@wanadoo.fr>
2020-04-20 11:55:29 +0200

    tag.h completer

--- code/tag.h
+++ code/tag.h
@@ -1,8 +1,32 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "liste.h"
+#incude "gestionTag.h"
+
+#ifndef TAG_H
+#define TAG_H
 
 struct fichier {
   int inode;
-  tag t;
+  char * path;
+  liste tag;
 };
-
 typedef struct fichier fichier;
 
+//Fonction de gestion de la liste des fichiers
+liste creerFichier (int inode, char *path, liste lFichier);
+liste suppFichier (fichier f, liste lFichier);
+fichier *getFichierI (int inode, liste lFichier);
+fichier *getFichierP (char *path, liste Lfichier);
+
+//Fonctions de gestion du lien fichier/tag
+fichier *ajouterTag (fichier f, liste t);
+fichier *retirerTag (fichier f, liste t);
+int estTaguer (fichier f, liste tListe, liste nTListe);
+fichier *changerPath (fichier f, char *newPath);
+fichier *cpFichier (fichier f, int newInode, char *newPath);
+
+
+#endif
