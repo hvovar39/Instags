8dbf2be5504a36c1ec218dc5d345d2ab617024c3
Hugo <hugo.vovard@wanadoo.fr>
2020-04-28 10:17:01 +0200

    travaille sur les commandes

--- code/InsTags.c
+++ code/InsTags.c
@@ -11,7 +11,8 @@ Contient la boucle d'interaction
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include "liste.h"
+#include "gestionFichier.h"
 
 /*DEFINE
 ================================================================
@@ -24,7 +25,8 @@ Contient la boucle d'interaction
 ================================================================
 */
 
-int ls (char *[]);
+int loadFic (liste, liste);
+int ls (char *[], size_t, liste);
 void init_arg (char *[]);
 void re_init_arg (char *[]);
 int sep_com (char *, char *[], int);
@@ -39,23 +41,31 @@ int main () {
   char *commande = malloc (500*sizeof(char));
   char *args[ARGC];
   init_arg (args);
-  printf (">");
+  
+
+  liste lTag = creer_liste();
+  liste lFic = creer_liste();
+  loadFic (lTag, lFic);
   
   while (fgets(commande, 500, stdin)) {
-    if ( (n = sep_com (commande, args, ARGC)) != 0){
+    if ( (n = sep_com (commande, args, ARGC)) == 0){
       //On separe la commande passer
-      
-      if ( (d = strcmp ("ls", args[0])) == 0)
-	ls (args);
-      
-      for (int i = 0; i<n; i++)
-	printf("%d -> %s!\n", i, args[i]);
-      
-      re_init_arg (args); //On re malloc ce qui a été free
-      printf (">");
-      memset (commande, 0, strlen(commande));
+      printf ("Oups, il y a eu un soucis avec la commande passé!\n");
+      return -1;
     }
+      
+    if ( (d = strcmp ("ls", args[0])) == 0)
+      ls (args, n, lTag);
+    
+    for (int i = 0; i<n; i++)
+      printf("%d -> %s!\n", i, args[i]);
+    
+    re_init_arg (args); //On re malloc ce qui a été free
+    printf (">");
+    memset (commande, 0, strlen(commande)); 
   }
+
+  
 
   return 0;
 }
@@ -64,11 +74,34 @@ int main () {
 /*FONCTIONS
 ================================================================
 */
-    
 
-int ls (char * argv[]) {
-  if (fork() == 0)
-    execvp ("ls", argv);
+int loadFic (liste lTag, liste lFic) {
+  //Chargement des listes de tag et de fichiers
+  char buff[50];
+  printf ("Souhaitez vous chargez les tags à partir des fichiers de sauvegarde ?\n >");
+  fgets (buff, 50, stdin);
+  if (buff[0] == 'y' && load (lTag, lFic, "TAG.csv", "FITAG.csv")){
+    printf ("Fichier bien chagré\n");
+    return 1;
+  }
+  printf ("Oups, il y a eu un soucis de chargement.\n");
+  return 0;
+}
+
+int ls (char * argv[], size_t t, liste lTag) {
+  //Execute ls, avec l'option spécifié. Traite l'option
+  if ( t<5 || strcmp (argv[1] == "-TAG")){
+    if (fork() == 0)
+      execvp ("ls", argv);
+    return 1;
+  } else {
+    liste conj = creer_liste();
+    liste neg = creer_liste();
+    for (int i = 3; i<t; i++) {
+      if (argv[i][0] == '!')
+	insere_apres (neg, getTag (argv[i]+1, lTag));
+    }
+  }
   return 1;
 }
 
